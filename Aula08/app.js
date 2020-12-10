637c28b9ea11576c10da38b5c94f31bdc6acd7ae
Claudinier576 <devneto576@gmail.com>
2020-12-10 20:35:12 -0300

    Servidor Criado e exibindo a Meu Servidor esta rodando

--- Aula08/app.js
+++ Aula08/app.js
@@ -1,1 +1,12 @@
+const http = require('http');
+
 console.log("Rodando o arquivo app.js");
+
+//res = resposta 
+//req = requisição
+
+http.createServer(function(req,res){
+    res.writeHead(200,{"Content-type": "text/plain"});
+    res.end("Meu Servidor esta rodando");
+    
+}).listen(8080,'localhost');
