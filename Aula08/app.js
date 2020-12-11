cefe69cc44f6da26d6cff185c59f49db6b5c86ef
Claudinier576 <devneto576@gmail.com>
2020-12-10 22:58:53 -0300

    Servidor Ligado e com mais do que uma url

--- Aula08/app.js
+++ Aula08/app.js
@@ -1,12 +1,37 @@
-const http = require('http');
-
-console.log("Rodando o arquivo app.js");
-
 //res = resposta 
 //req = requisição
+const http = require('http');
+const productsRoom = require('./produtos')
+console.log("Rodando o arquivo app.js");
 
 http.createServer(function(req,res){
-    res.writeHead(200,{"Content-type": "text/plain"});
-    res.end("Meu Servidor esta rodando");
-    
+   console.log(req.url)
+   switch(req.url){
+    case "/produtos" :
+        getProducts(res);
+        break;
+        case "/produtos/casa" :
+            getProductshome(res);
+            break;
+        default: 
+        defaultRoute(res);  
+   }  
 }).listen(8080,'localhost');
+function defaultRoute(response){
+    response.writeHead(200,{"Content-type": "text/plain"});
+    response.end("Bem Vindo ao Servidor!")
+}
+function getProducts(response){
+    response.writeHead(200,{"Content-type": "text/plain"});
+    const stringProducts = JSON.stringify(productsRoom);
+    response.end(stringProducts);
+}
+function getProductshome(response){
+    response.writeHead(200,{"Content-type": "text/plain"});
+    response.end("Televisao" +"\n" + "Monitor"+"\n"  + "Mesa");
+}
+
+function getProductsInfo(response){
+    response.writeHead(200,{"Content-type": "text/plain"});
+    response.end("Cama" +"\n" + "Armario"+"\n"  + "Cortina" + "\n" + "Tapetes");
+}
---
+++ Aula08/produtos.js
@@ -0,0 +1,35 @@
+const productsRoom = [
+    {
+        name: 'Intel i5 setima geracao',
+        price : 1350
+    },{
+        name: 'RTX 3080',
+        price : 5400
+    },
+    {
+        name: 'Teclado dazz',
+        price : 350
+    },
+    {
+        name: 'Teclado rezer',
+        price : 780
+    },
+    {
+        name: 'Mouse xZone 800-16000dpi',
+        price : 180
+    },
+    {
+        name: 'HeadSet HP',
+        price : 160
+    },
+    {
+        name: 'Microfone dazz',
+        price : 350
+    },
+    {
+        name: 'Teclado dazz',
+        price : 350
+    }
+]
+
+module.exports = productsRoom;
